756edaaf4b142d60679ad8049984800ad4265eb6
leff0506 <leff0506@gmail.com>
2020-02-10 17:36:47 +0200

    test

--- claster.py
+++ claster.py
@@ -3,16 +3,18 @@ import json
 import numpy as np
 import base64
 import deskewing as desk
+
+
 def detect_text(origin):
     url = 'https://vision.googleapis.com/v1/images:annotate?alt=json&key=AIzaSyAwy6okZ-wrLFCehajOsN8S9fn_4d3eoWI'
     with open(origin, "rb") as img_file:
-        base= base64.b64encode(img_file.read())
+        base = base64.b64encode(img_file.read())
         base = base.decode('utf-8')
     req = '{"requests": [{"image": {"content":"'
     req += base
-    req +='"},"features": [{"type": "TEXT_DETECTION","model": "builtin/latest"}],"imageContext": {"languageHints": "en"}}]}'
+    req += '"},"features": [{"type": "TEXT_DETECTION","model": "builtin/latest"}],"imageContext": {"languageHints": "en"}}]}'
     headers = {'Content-type': 'application/json',
-               'Accept': 'text/plain',}
+               'Accept': 'text/plain', }
     answer = requests.post(url, data=req, headers=headers)
     answer = answer.json()
     return answer
@@ -90,12 +92,14 @@ def json_to_rects(js):
         rects.append(rect(temp[0], temp[1], temp[2], temp[3]))
         rects[i - 1].set_word(data[i]['description'])
     return rects
+
+
 def dict_to_np(rects):
     X = [[0 for i in range(2)] for j in range(len(rects))]
     for i in range(len(rects)):
         X[i][0] = int(rects[i].center['x'])
         X[i][1] = int(rects[i].center['y'])
-    X = np.array(X,dtype = int)
+    X = np.array(X, dtype=int)
     return X
 
 
@@ -113,13 +117,14 @@ def filt(rects):
     #         h = r.p3['y'] - r.p1['y']
     #         print(r.word, h)
     #         break
-    h = 13
+    h = 8
     res = []
     eps = 0
-    forbiden = ["consignee", "vessel", "voyage", "no."]
+    forbiden = ["consignee", "vessel", "voyage", "no.", "port"]
     # forbiden = []
     for r in rects:
-        if r.p3.get('y',0) - r.p1.get('y',1e7) >= h - eps and 'x' in r.p1 and 'y' in r.p1 and 'x' in r.p2 and 'y' in r.p2 and 'x' in r.p3 and 'y' in r.p3 and 'x' in r.p4 and 'y' in r.p4 and not r.word.lower() in forbiden:
+        if r.p3.get('y', 0) - r.p1.get('y',
+                                       1e7) >= h - eps and 'x' in r.p1 and 'y' in r.p1 and 'x' in r.p2 and 'y' in r.p2 and 'x' in r.p3 and 'y' in r.p3 and 'x' in r.p4 and 'y' in r.p4 and not r.word.lower() in forbiden:
             res.append(r)
 
     return res
@@ -169,56 +174,93 @@ def result_claster(X, labels, photo, rects, save='claster_res.png'):
                                               facecolor=col[i % len(col)]))
     plt.savefig(save)
     # plt.show()
-def dist_rect(a ,b):
+
+
+def dist_rect(a, b):
     w = 1e7
     h = 1e7
     if (a.p1['x'] <= b.p1['x'] <= a.p3['x']) or (a.p1['x'] <= b.p3['x'] <= a.p3['x']) or (
             b.p1['x'] <= a.p1['x'] <= b.p3['x']) or (b.p1['x'] <= a.p3['x'] <= b.p3['x']):
         w = 0
     else:
-        w = min(w,abs(a.p1['x'] - b.p1['x']))
-        w = min(w,abs(a.p1['x'] - b.p3['x']))
-        w = min(w,abs(a.p3['x'] - b.p3['x']))
-        w = min(w,abs(a.p3['x'] - b.p1['x']))
-        w = max(w,0)
+        w = min(w, abs(a.p1['x'] - b.p1['x']))
+        w = min(w, abs(a.p1['x'] - b.p3['x']))
+        w = min(w, abs(a.p3['x'] - b.p3['x']))
+        w = min(w, abs(a.p3['x'] - b.p1['x']))
+        w = max(w, 0)
     if (a.p1['y'] <= b.p1['y'] <= a.p3['y']) or (a.p1['y'] <= b.p3['y'] <= a.p3['y']) or (
             b.p1['y'] <= a.p1['y'] <= b.p3['y']) or (b.p1['y'] <= a.p3['y'] <= b.p3['y']):
         h = 0
     else:
-        h = min(h,abs(a.p1['y'] - b.p1['y']))
-        h = min(h,abs(a.p1['y'] - b.p3['y']))
-        h = min(h,abs(a.p3['y'] - b.p3['y']))
-        h = min(h,abs(a.p3['y'] - b.p1['y']))
-        h = max(h,0)
-    if w== 0 and h == 0:
-        w=h=1
-    return (w,h)
+        h = min(h, abs(a.p1['y'] - b.p1['y']))
+        h = min(h, abs(a.p1['y'] - b.p3['y']))
+        h = min(h, abs(a.p3['y'] - b.p3['y']))
+        h = min(h, abs(a.p3['y'] - b.p1['y']))
+        h = max(h, 0)
+    if w == 0 and h == 0:
+        w = h = 1
+    return (w, h)
 
 
-def result_dfs(rects, width, height, lines=[]):
+def result_dfs(rects, width, height, vert=[], hor=[]):
     from collections import defaultdict
     n = len(rects)
 
-    def collision_one(a, b, line):
+    def collision_vert_one(a, b, line):
         (w, h) = dist_rect(a, b)
         if w == 0:
             return False
-        if ((line[1] <= a.center['y'] <= line[2]) or (line[1] <= b.center['y'] <= line[2])) and min(a.center['x'], b.center['x']) <= line[0] and max(a.center['x'], b.center['x']) >= line[0]:
+        if ((line[1] <= a.center['y'] <= line[2]) or (line[1] <= b.center['y'] <= line[2])) and min(a.center['x'],
+                                                                                                    b.center['x']) <= \
+                line[0] and max(a.center['x'], b.center['x']) >= line[0]:
             return True
-        elif ((line[1] <= a.p1['y'] <= line[2]) or (line[1] <= b.p1['y'] <= line[2])) and min(a.p1['x'], b.p1['x']) <= line[0] and max(a.p1['x'], b.p1['x']) >= line[0]:
+        elif ((line[1] <= a.p1['y'] <= line[2]) or (line[1] <= b.p1['y'] <= line[2])) and min(a.p1['x'], b.p1['x']) <= \
+                line[0] and max(a.p1['x'], b.p1['x']) >= line[0]:
             return True
-        elif ((line[1] <= a.p3['y'] <= line[2]) or (line[1] <= b.p3['y'] <= line[2])) and min(a.p3['x'], b.p3['x']) <= line[0] and max(a.p3['x'], b.p3['x']) >= line[0]:
+        elif ((line[1] <= a.p3['y'] <= line[2]) or (line[1] <= b.p3['y'] <= line[2])) and min(a.p3['x'], b.p3['x']) <= \
+                line[0] and max(a.p3['x'], b.p3['x']) >= line[0]:
             return True
-        elif ((line[1] <= a.p3['y'] <= line[2]) or (line[1] <= b.p1['y'] <= line[2])) and min(a.p3['x'], b.p1['x']) <= line[0] and max(a.p3['x'], b.p1['x']) >= line[0]:
+        elif ((line[1] <= a.p3['y'] <= line[2]) or (line[1] <= b.p1['y'] <= line[2])) and min(a.p3['x'], b.p1['x']) <= \
+                line[0] and max(a.p3['x'], b.p1['x']) >= line[0]:
             return True
-        elif ((line[1] <= a.p1['y'] <= line[2]) or (line[1] <= b.p3['y'] <= line[2])) and min(a.p1['x'], b.p3['x']) <= line[0] and max(a.p1['x'], b.p3['x']) >= line[0]:
+        elif ((line[1] <= a.p1['y'] <= line[2]) or (line[1] <= b.p3['y'] <= line[2])) and min(a.p1['x'], b.p3['x']) <= \
+                line[0] and max(a.p1['x'], b.p3['x']) >= line[0]:
             return True
         else:
             return False
 
-    def collision(a, b, lines):
+    def collision_vert(a, b, lines):
         for line in lines:
-            if collision_one(a, b, line):
+            if collision_vert_one(a, b, line):
+                return True
+        return False
+
+    def collision_hor_one(a, b, line):
+        (w, h) = dist_rect(a, b)
+        if h == 0:
+            return False
+        if ((line[1] <= a.center['x'] <= line[2]) or (line[1] <= b.center['x'] <= line[2])) and min(a.center['y'],
+                                                                                                    b.center['y']) <= \
+                line[0] and max(a.center['y'], b.center['y']) >= line[0]:
+            return True
+        elif ((line[1] <= a.p1['x'] <= line[2]) or (line[1] <= b.p1['x'] <= line[2])) and min(a.p1['y'], b.p1['y']) <= \
+                line[0] and max(a.p1['y'], b.p1['y']) >= line[0]:
+            return True
+        elif ((line[1] <= a.p3['x'] <= line[2]) or (line[1] <= b.p3['x'] <= line[2])) and min(a.p3['y'], b.p3['y']) <= \
+                line[0] and max(a.p3['y'], b.p3['y']) >= line[0]:
+            return True
+        elif ((line[1] <= a.p3['x'] <= line[2]) or (line[1] <= b.p1['x'] <= line[2])) and min(a.p3['y'], b.p1['y']) <= \
+                line[0] and max(a.p3['y'], b.p1['y']) >= line[0]:
+            return True
+        elif ((line[1] <= a.p1['x'] <= line[2]) or (line[1] <= b.p3['x'] <= line[2])) and min(a.p1['y'], b.p3['y']) <= \
+                line[0] and max(a.p1['y'], b.p3['y']) >= line[0]:
+            return True
+        else:
+            return False
+
+    def collision_hor(a, b, lines):
+        for line in lines:
+            if collision_hor_one(a, b, line):
                 return True
         return False
 
@@ -255,7 +297,8 @@ def result_dfs(rects, width, height, lines=[]):
             if not i == j:
                 (w, h) = dist_rect(rects[i], rects[j])
 
-                if w <= width and h <= height and not collision(rects[i], rects[j], lines):
+                if w <= width and h <= height and not collision_vert(rects[i], rects[j], vert) and not collision_hor(
+                        rects[i], rects[j], hor):
                     g.addEdge(i, j)
     #                     print(i,j,w,h)
     # print(g.graph[11])
@@ -271,74 +314,140 @@ def result_dfs(rects, width, height, lines=[]):
     return g.labels
 
 
-def get_vert_lines(rects, labels):
+def get_lines(rects, labels):
     points = []
     for i in range(len(np.unique(labels))):
         x = 1e7
         y1 = 1e7
         y2 = 1e7
+        word = ""
         for j in range(len(rects)):
             if labels[j] == i:
+                if word == "":
+                    word = rects[j].word
                 x = min(x, rects[j].p1['x'])
                 y1 = min(y1, rects[j].p1['y'])
                 y2 = min(y2, rects[j].p3['y'])
-        points.append([x, y1])
-        points.append([x, y2])
+        points.append([x, y1, i, word])
+        points.append([x, y2, i, word])
     points = sorted(points)
     #     for p in points:
     #         print(p[0],p[1])
-    x_eps = 10
+    x_eps = 20
     y_eps = 50
     result = []
+    has = []
     was = []
     was.append(1)
     was_treshhold = 3
     j = 0
+    # print("new", points[0])
     result.append([points[0][0], points[0][1], points[0][1]])
+    has.append([points[0][2]])
     for i in range(1, len(points)):
         done = False
+        pos_k = -1
+        l = 1e7
         for k in range(len(result)):
-            if abs(points[i][0] - result[k][0]) <= x_eps and abs(points[i][1] - result[k][2]) <= y_eps:
-                done = True
-                was[k] += 1
-                result[k][2] = max(result[k][2], points[i][1])
-                break
+            if abs(points[i][0] - result[k][0]) <= x_eps and min(abs(points[i][1] - result[k][2]),
+                                                                 abs(points[i][1] - result[k][1])) <= y_eps:
+                if min(abs(points[i][1] - result[k][2]), abs(points[i][1] - result[k][1])) < l:
+                    pos_k = k
+                    l = min(abs(points[i][1] - result[k][2]), abs(points[i][1] - result[k][1]))
+
+        if pos_k != -1:
+            done = True
+            was[pos_k] += 1
+            has[pos_k].append(points[i][2])
+            result[pos_k][2] = max(result[pos_k][2], points[i][1])
+            result[pos_k][1] = min(result[pos_k][1], points[i][1])
+            # print(points[i], "added to", result[pos_k])
+
         if not done:
             was.append(1)
+            has.append([points[i][2]])
             result.append([points[i][0], points[i][1], points[i][1]])
+            # print("new", points[i])
     answer = []
-    y1_eps = 5
-    y2_eps = 10
+    has_answer = []
+    y1_eps = 10
+    y2_eps = 40
     for i in range(len(result)):
         if was[i] >= was_treshhold:
             result[i][1] = max(0, result[i][1] - y1_eps)
             result[i][2] += y2_eps
             answer.append(result[i])
-    return answer
-def self_recorded(origin , dest):
-    print("start :",origin)
+            has_answer.append(has[i])
+    union = []
+    has_union = []
+    result = answer
+    has = has_answer
+    union.append(result[0])
+    has_union.append(has[0])
+    x_eps = 20
+    y_eps = 50
+    for i in range(1, len(result)):
+        done = False
+        for k in range(len(union)):
+            if abs(result[i][0] - union[k][0]) <= x_eps and min(abs(result[i][1] - union[k][1]),
+                                                                abs(result[i][1] - union[k][2]),
+                                                                abs(result[i][2] - union[k][1]),
+                                                                abs(result[i][2] - union[k][2])) <= y_eps:
+                done = True
+                for it in has[i]:
+                    has_union[k].append(it)
+                union[k][1] = min(union[k][1], result[i][1], result[i][2])
+                union[k][2] = max(union[k][2], result[i][1], result[i][2])
+                break
+        if not done:
+            union.append(result[i])
+            has_union.append(has[i])
+
+    hor_lines = []
+
+    for i in range(len(union)):
+        hor_lines.append([union[i][1], union[i][0], union[i][0]])
+        for label in has_union[i]:
+            for j in range(len(rects)):
+                if labels[j] == label:
+                    hor_lines[i][2] = max(hor_lines[i][2], rects[j].p3['x'])
+    return [union, hor_lines]
+
+
+def self_recorded(origin, dest):
+    print("start :", origin)
     rects = filt(json_to_rects(detect_text(origin)))
     X = dict_to_np(rects)
-    labels = result_dfs(rects,20,0)
-    lines = get_vert_lines(rects,labels)
+    labels = result_dfs(rects, 20, 0)
+    lines = get_lines(rects, labels)
     # lines = []
-    # draw_lines(origin,lines)
-#     print(type(rects))
-    #25 15
-    result_claster(X,result_dfs(rects,25,15,lines),origin,rects,save = dest)
+    lines[1] = []
+    # draw_lines(origin, lines[0], lines[1])
+    #     print(type(rects))
+    # 25 15
+    result_claster(X,result_dfs(rects,25,15,lines[0],lines[1]),origin,rects,save = dest)
     print("end :", origin)
-def draw_lines(photo,lines):
+
+
+def draw_lines(photo, vert, hor):
     import matplotlib.pyplot as plt
-    col = [[0,0,0,0.5],[0,1,0,0.5],[0,0,1,0.5],[1,0,1,0.5],[1,1,0,0.5],[0,1,1,0.5],[1,0,0,0.1],[0.5,0.5,0,0.5]]
-    plt.figure(figsize=(30,40))
+    col = [[0, 0, 0, 1], [0, 1, 0, 1], [0, 0, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [0, 1, 1, 1], [1, 0, 0, 1],
+           [0.5, 0.5, 0, 1]]
+    plt.figure(figsize=(30, 40))
     image = plt.imread(photo)
     plt.imshow(image)
-    for i in range(len(lines)):
-        x_values = 2*[lines[i][0]]
-        y_values = [lines[i][1],lines[i][2]]
-        plt.plot(x_values,y_values,c = col[i%len(col)],linewidth=8)
+    for i in range(len(vert)):
+        x_values = 2 * [vert[i][0]]
+        y_values = [vert[i][1], vert[i][2]]
+        plt.plot(x_values, y_values, c=col[i % len(col)], linewidth=3)
+        # y_values = 2 * [hor[i][0]]
+        # x_values = [hor[i][1], hor[i][2]]
+        # plt.plot(x_values, y_values, c=col[i % len(col)], linewidth=3)
     plt.show()
+
+
 import os
+
 # directory = './test/samples'
 # dest = "./test/norm/"
 # for filename in os.listdir(directory):
@@ -353,4 +462,4 @@ for filename in os.listdir(directory):
 # print(rects[5],rects[11])
 # print(dist_rect(rects[5],rects[11]))
 # result_dfs(rects,20, 0)
-# self_recorded("./test/norm/data.png","./test/self_recorded/data.png")
+# self_recorded("./test/norm/data.png", "./test/self_recorded/data.png")
